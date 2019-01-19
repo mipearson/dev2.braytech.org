e991d89cc3135c3190b0c07ed47a8bc4f47b8f9c
Michael Pearson <mipearson@gmail.com>
2019-01-19 13:55:08 +1100

    Tidy up bungie.js, add profile methods

--- src/utils/bungie.js
+++ src/utils/bungie.js
@@ -7,26 +7,17 @@ async function apiRequest(path) {
   const request = await fetch(`https://www.bungie.net${path}`, options).then(r => r.json());
 
   if (request.ErrorCode !== 1) {
-    throw `Error retrieving ${path} from Bungie`;
+    throw `Error retrieving ${path} from Bungie: (${request.ErrorStatus} code ${request.ErrorCode}) ${request.Message}`;
   }
 
   return request.Response;
 }
 
-async function manifestIndex() {
-  return apiRequest('/Platform/Destiny2/Manifest/');
-}
-
-async function settings() {
-  return apiRequest('/Platform/Settings/');
-}
-
-async function manifest(version) {
-  return fetch(`https://www.bungie.net${version}`).then(a => a.json());
-}
-
 export default {
-  manifestIndex,
-  settings,
-  manifest
+  manifestIndex: async () => apiRequest('/Platform/Destiny2/Manifest/'),
+  settings: async () => apiRequest(`/Platform/Settings`),
+  milestones: async () => apiRequest('/Platform/Destiny2/Milestones'),
+  manifest: async version => fetch(`https://www.bungie.net${version}`).then(a => a.json()),
+  memberProfile: async (membershipType, membershipId) => apiRequest(`/Platform/Destiny2/${membershipType}/Profile/${membershipId}/?components=100,104,200,202,204,205,300,301,302,303,304,305,800,900`),
+  memberGroups: async (membershipType, membershipId) => apiRequest(`/Platform/GroupV2/User/${membershipType}/${membershipId}/0/1/`)
 };
